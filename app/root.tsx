57dc083ac8562e70e66b7c6de489656dfe0dcd03
Kent Ntwari <designerjehovah@gmail.com>
2024-09-18 12:38:34 -0400

    styles:add woff2 fonts

--- app/root.tsx
+++ app/root.tsx
@@ -19,8 +19,16 @@ export const links: LinksFunction = () => [
     crossOrigin: "anonymous",
   },
   {
-    rel: "stylesheet",
-    href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
+    rel: "preload",
+    href: "/fonts/mersad-regular.woff2",
+  },
+  {
+    rel: "preload",
+    href: "/fonts/mersad-medium.woff2",
+  },
+  {
+    rel: "preload",
+    href: "/fonts/mersad-bold.woff2",
   },
 ];
 
